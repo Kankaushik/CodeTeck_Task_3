73ed8f272f57f77d726960fd2e8823708e2137b5
Kanchan Kumari <kankaushik7@gmail.com>
2025-07-04 16:34:42 +0530

    Add

---
+++ scripts.js
@@ -0,0 +1,49 @@
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": { "enable": true, "value_area": 800 }
+    },
+    "color": {
+      "value": ["#ffffff", "#e94560", "#ff6679", "#66ff66", "#66ccff"]
+    },
+    "shape": {
+      "type": "star", // Valid shape
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true
+    },
+    "size": {
+      "value": 4,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#aaffc3",
+      "opacity": 0.5,
+      "width": 1.5
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        
+      }
+    }
+  },
+  "retina_detect": true
+});
+
